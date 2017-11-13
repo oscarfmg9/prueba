5190385d524b86db0c98b1ce91c3fe33735d9e2f
Oscar Fabian Maldonado Garcia <oscarf.maldonadog@gmail.com>
2017-11-13 13:03:40 -0600

    New Functions to call weather with the city or with coordinates
    Signed-off-by: Oscar Fabian Maldonado Garcia <oscarf.maldonadog@gmail.com>

--- src/CallWeather/callApi.php
+++ src/CallWeather/callApi.php
@@ -14,27 +14,57 @@ class callApi
         echo "</pre>";
     }
 
-    function setUrl($appId,$city){
+    function setCityUrl($appId,$city){
         $url = 'http://api.openweathermap.org/data/2.5/weather?APPID='.$appId.'&q='.$city.'';
         return $url;
     }
 
-    function setAppId(){
-        $appID = '678777737d2cc5556ccedfbbae776196';
-        return $appID;
+    function setCoorUrl($appId,$lat,$lon){
+        $url = 'api.openweathermap.org/data/2.5/weather?APPID='.$appId.'&lat='.$lat.'&lon='.$lon.'';
+        return $url;
     }
 
-    function curlInit($city){
+    function setAppId($userAppId){
+        global $appId;
+        $appId = $userAppId;
+        return $appId;
+    }   
+
+    function getAppId(){
+        global $appId;
+        $appId = $appId;
+        return $appId;
+    }   
+
+    function curlExecute($ch){
+        $response = curl_exec($ch);
+        $response  = json_decode($response);
+        curl_close($ch);
+        return $response;
+    }
+
+    function curlInit($url){
         $ch=curl_init();
-        $appID = callApi::setAppId();
-        $url = callApi::setUrl($appID,$city);
         curl_setopt($ch, CURLOPT_URL,$url);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $response = curl_exec($ch);
-        $response  = json_decode($response);
-        callApi::pr($response);
-        curl_close($ch);
+        return $ch;
+    }
+
+    function getCityWeather($city){
+        $appID = callApi::getAppId();
+        $url = callApi::setCityUrl($appID,$city);
+        $curl_handler = callApi::curlInit($url);
+        $response = callApi::curlExecute($curl_handler);
+        return $response;
+    }
+
+    function getCoorWeather($lat,$lon){
+        $appID = callApi::getAppId();
+        $url = callApi::setCoorUrl($appID,$lat,$lon);
+        $curl_handler = callApi::curlInit($url);
+        $response = callApi::curlExecute($curl_handler);
+        return $response;
     }
 }
 ?>
--- tests/test.php
+++ tests/test.php
@@ -5,4 +5,9 @@ require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Compos
 use CallWeather\callApi;
 
 $city  = 'London';
-callApi::curlInit($city);
+$appID = callApi::setAppId('678777737d2cc5556ccedfbbae776196');
+$lat = '35';
+$lon = '139';
+$weatherCoor = callApi::getCoorWeather($lat,$lon);
+$weatherCity = callApi::getCityWeather($city);
+callApi::pr($weatherCity);
